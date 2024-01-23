86e6b2fad37a9f8f60ab9e9c0fd944b7533500f2
Diego Sogari <diego.sogari@gmail.com>
2024-01-23 11:20:07 -0300

    Fix tsconfig include

--- dist/binding.d.ts
+++ dist/binding.d.ts
@@ -1,9 +1,7 @@
 interface InitOptions {
   context: import("@emnapi/runtime").Context;
-
   /** node_api_get_module_file_name */
   filename?: string;
-
   /**
    * Support following async_hooks related things
    * on Node.js runtime only
@@ -15,13 +13,10 @@ interface InitOptions {
    * napi_create_async_work and napi_create_threadsafe_function
    */
   nodeBinding?: typeof import("@emnapi/node-binding");
-
   /** See Multithread part */
   asyncWorkPoolSize?: number;
 }
-
 interface Module {
   emnapiInit(options: InitOptions): typeof import("./cola.js");
 }
-
 export default function (moduleDefaults?: object): Promise<Module>;
